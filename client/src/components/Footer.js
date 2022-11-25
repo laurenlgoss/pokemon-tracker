c6196db6da0c1ce1ee64577cd152c7c042d53084
Lauren Goss <laurenlgoss98@gmail.com>
2022-11-25 12:21:15 -0500

    updated footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -2,25 +2,60 @@ import React from 'react';
 import { useLocation } from 'react-router-dom';
 
 const styles = {
-  footer: {
-    maxHeight: '30px',
-  },
   footerText: {
     color: '#ababab',
+  },
+  footerLink: {
+    color: 'var(--secondary)',
+    textDecoration: 'underline',
+  },
+  footerDivider: {
+    color: 'var(--secondary)',
   },
 };
 
 function Footer() {
   return (
     <footer
-      style={styles.footer}
       className={useLocation().pathname === '/' ? 'fixed-bottom' : null} // Footer is fixed to bottom only if url is '/'
     >
-      <div className="row">
-        <div className="col text-center">
-          <p style={styles.footerText}>
-            © Lauren Goss {new Date().getFullYear()}
-          </p>
+      <div className="row my-3">
+        <div className="col-12 text-center">
+          <div style={styles.footerText}>
+            © {new Date().getFullYear()} Lauren Goss
+            <br />
+            <small>
+              <a href="/" style={styles.footerLink}>
+                Home
+              </a>
+              <span className="mx-3" style={styles.footerDivider}>
+                |
+              </span>
+              <a
+                target="_blank"
+                href="https://github.com/laurenlgoss/pokemon-tracker/issues"
+                style={styles.footerLink}
+                rel="noreferrer"
+              >
+                Report Issues
+              </a>
+              <span className="mx-3" style={styles.footerDivider}>
+                |
+              </span>
+              {/* <a href="#" style={styles.footerLink}>
+                Privacy Policy
+              </a>
+              <span className="mx-3">|</span> */}
+              <a
+                target="_blank"
+                href="https://laurenlgoss.github.io/lauren-goss-portfolio-react/"
+                style={styles.footerLink}
+                rel="noreferrer"
+              >
+                About Me
+              </a>
+            </small>
+          </div>
         </div>
       </div>
     </footer>
